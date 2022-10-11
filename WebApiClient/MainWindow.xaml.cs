0399d511d37edf761277c1b5d7afbeec39441460
gavand-sagar <gavand.sagar.s@gmail.com>
2022-10-11 15:21:26 +0530

    Add project files.

---
+++ WebApiClient/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WebApiClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HttpClient httpClient;
+        public MainWindow()
+        {
+            httpClient = new HttpClient();
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var result = httpClient.GetAsync($"https://localhost:44357/api/packages?name={txtSeach.t}")
+                .Result
+                .Content
+                .ReadAsAsync<IEnumerable<Package>>()
+                .Result;
+
+            lstbxPackages.ItemsSource = result;
+        }
+
+
+    }
+
+    public class Package
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    class CatFact
+    {
+        public string Fact { get; set; }
+        public int Length { get; set; }
+    }
+
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    }
+}
---
+++ WebApplication1/Controllers/CustomerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
